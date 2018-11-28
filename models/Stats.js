a4dda85d3c731fc4ae4fb670dd7ef515de7ae8cd
Gregor Aisch <gka@users.noreply.github.com>
2018-11-28 17:26:53 +0100

    added Stats + test

---
+++ models/Stats.js
@@ -0,0 +1,27 @@
+const SQ = require('sequelize');
+const {db} = require('../index');
+
+const Stats = db.define('stats', {
+
+    id: {
+        type: SQ.INTEGER,
+        primaryKey:true,
+        autoIncrement: true
+    },
+
+    metric: {
+        type: SQ.STRING,
+        allowNull: false,
+    },
+
+    value: {
+        type: SQ.INTEGER,
+        allowNull: false,
+    }
+
+}, {
+    createdAt: 'time',
+    tableName: 'stats',
+});
+
+module.exports = Stats;
--- models/index.js
+++ models/index.js
@@ -5,6 +5,7 @@ const models = {};
 	'Chart',
 	'ExportJob',
 	'Folder',
+    'Stats',
 	'Team',
 	'Theme',
 	'User',
