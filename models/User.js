c3da778d0b21150bbb85637119469706406aaa1b
Gregor Aisch <gka@users.noreply.github.com>
2018-11-30 23:15:40 +0100

    added user.mayUsePlugin

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const SQ = require('sequelize');
 const {db} = require('../index');
+const {uniq} = require('underscore');
 
 const User = db.define('user', {
 
@@ -53,6 +54,39 @@ User.prototype.canEditChart = async function(chart) {
     if (this.role == 'admin' || this.role == 'sysadmin') return true;
     // the user is member of a team the chart belongs to
     return await this.hasTeam(chart.organization_id);
+};
+
+/*
+ * get list of all products a user has access to
+ * through UserProduct or TeamProducts
+ */
+User.prototype.getAllProducts = async function() {
+    const products = await this.getProducts();
+    const teams = await this.getTeams();
+    if (teams.length) {
+        for (let team of teams) {
+            const team_products = await team.getProducts();
+            products.push.apply(products, team_products);
+        }
+    }
+    return uniq(products).sort((a,b) => a.priority - b.priority);
+};
+
+/*
+ * decides whether or not a user may use functions provided
+ * by a certain plugin. intended to be used by the plugins
+ *
+ * @returns true|false
+ */
+User.prototype.mayUsePlugin = async function(plugin_id) {
+    const plugin = await Plugin.findByPk(plugin_id);
+    if (!plugin.is_private) return true;
+    // look through all the products of this user
+    const products = await this.getAllProducts();
+    for (let product of products) {
+        if (product.hasPlugin(plugin_id)) return true;
+    }
+    return false;
 };
 
 module.exports = User;
