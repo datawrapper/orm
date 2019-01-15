d15d3e68049cf55676d2209d3a622b4289779a02
Gregor Aisch <gka@users.noreply.github.com>
2019-01-15 13:03:04 +0100

    dw setup project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true
+    env: {
+        browser: true,
+        commonjs: true
     },
-    "parser": "babel-eslint",
-    "extends": "eslint:recommended",
-    "globals": {
-        "Reflect": true
+    parser: 'babel-eslint',
+    extends: ['standard'],
+    globals: {
+        Reflect: true
     },
-    "rules": {
-        "no-sequences": "error",
-        "eqeqeq": ["error", "smart"],
-        "no-multiple-empty-lines": ["error", { "max": 2 }],
-        "no-console": ["error", { allow: ["warn", "error"] }]
+    rules: {
+        'no-sequences': 'error',
+        eqeqeq: ['error', 'smart'],
+        'no-multiple-empty-lines': ['error', { max: 2 }],
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+        indent: ['error', 4],
+        semi: ['error', 'always'],
+        camelcase: ['off']
     }
 };
-
