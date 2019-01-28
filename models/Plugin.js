db414ba4c7df3aafe1fdbc1862e9982a3564762b
Gregor Aisch <gka@users.noreply.github.com>
2019-01-28 15:29:17 +0100

    don't remove any existing plugins for now

--- models/Plugin.js
+++ models/Plugin.js
@@ -23,17 +23,7 @@ const Plugin = db.define(
  * up in the plugin database table
  */
 Plugin.register = async function (app, plugins) {
-    const PluginData = require('./PluginData');
-
-    // first remove all plugins registered prei
-    await PluginData.destroy({
-        where: {
-            key: 'installed_by',
-            data: app
-        }
-    });
-
-    // then make sure the plugins are in the plugin list
+    // make sure the plugins are in the plugin list
     await Plugin.bulkCreate(
         plugins.map(p => {
             return {
@@ -44,29 +34,6 @@ Plugin.register = async function (app, plugins) {
         }),
         { ignoreDuplicates: true }
     );
-
-    // add installed_at entries for this app
-    await PluginData.bulkCreate(
-        plugins.map(p => {
-            return {
-                plugin_id: p,
-                key: 'installed_by',
-                data: app
-            };
-        })
-    );
-
-    // remove plugins that where not installed by any other app
-    const remove = (await Plugin.findAll({ where: { id: { [SQ.Op.notIn]: plugins } } })).map(
-        d => d.id
-    );
-    const keep = await PluginData.findAll({
-        where: { plugin_id: { [SQ.Op.in]: remove }, key: 'installed_by' }
-    });
-    keep.forEach(p => {
-        remove.splice(remove.indexOf(p.plugin_id), 1);
-    });
-    await Plugin.destroy({ where: { id: { [SQ.Op.in]: remove } } });
 };
 
 module.exports = Plugin;
