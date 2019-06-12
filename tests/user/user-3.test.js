7dae3a9b0953242a96113184eb87fd08c9538d99
Gregor Aisch <gka@users.noreply.github.com>
2019-06-12 09:55:31 +0200

    add test for selecting users with and ordering by chart count

---
+++ tests/user/user-3.test.js
@@ -0,0 +1,41 @@
+const test = require('ava');
+const { close, init } = require('../index');
+/*
+ * user 2 is an editor who has one chart
+ */
+
+test.before(async t => {
+    await init();
+    const { db } = require('../../index');
+    const { User, Chart } = require('../../models');
+    t.context = await User.findAll({
+        include: [
+            {
+                model: Chart,
+                required: false,
+                where: { author_id: db.col('user.id') }
+            }
+        ],
+        group: ['user.id'],
+        attributes: {
+            include: [[db.fn('COUNT', db.col('charts.id')), 'chart_count']]
+        },
+        order: [[db.fn('COUNT', db.literal('charts.id')), 'DESC']]
+    });
+});
+
+test('found one user with a single chart', t => {
+    // 3 users in total
+    t.is(t.context.length, 3);
+    // first has two charts
+    t.is(t.context[0].get('email'), 'ci3@datawrapper.de');
+    t.is(t.context[0].get('chart_count'), 2);
+    // second has one chart
+    t.is(t.context[1].get('email'), 'ci2@datawrapper.de');
+    t.is(t.context[1].get('chart_count'), 1);
+    // third has no charts
+    t.is(t.context[2].get('email'), 'ci1@datawrapper.de');
+    t.is(t.context[2].get('chart_count'), 0);
+});
+
+test.after(t => close);
