a278b466d5409f4b673b52357264a51440c53828
David Kokkelink <david@datawrapper.de>
2019-10-21 02:20:29 +0200

    add invalidatePluginCache helper for Team model

--- models/Team.js
+++ models/Team.js
@@ -21,6 +21,29 @@ const Team = db.define(
 );
 
 const Theme = require('./Theme');
+const UserTeam = require('./UserTeam');
+const UserPluginCache = require('./UserPluginCache');
+
 Team.belongsTo(Theme, { foreignKey: 'default_theme' });
+
+Team.prototype.invalidatePluginCache = async function() {
+    const userTeams = await UserTeam.findAll({
+        where: {
+            organization_id: this.id
+        }
+    });
+
+    let userQuery = { [SQ.Op.or]: [] };
+
+    for (let userTeam of userTeams) {
+        userQuery[SQ.Op.or].push({
+            user_id: userTeam.user_id
+        });
+    }
+
+    await UserPluginCache.destroy({
+        where: userQuery
+    });
+};
 
 module.exports = Team;
