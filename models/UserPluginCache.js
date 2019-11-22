74f18b1389ee8b4dec599c7a5320030a274cc855
Fabian Gündel <f.guendel@gmail.com>
2019-11-22 11:46:47 +0100

    fix: add primary key to model

--- models/UserPluginCache.js
+++ models/UserPluginCache.js
@@ -4,6 +4,10 @@ const { db } = require('../index');
 const UserPluginCache = db.define(
     'user_plugin_cache',
     {
+        user_id: {
+            type: SQ.INTEGER,
+            primaryKey: true
+        },
         plugins: SQ.TEXT
     },
     {
