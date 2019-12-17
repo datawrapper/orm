5067da96fbe72d5962f38c474072f3ed3d2bd851
Gregor Aisch <gka@users.noreply.github.com>
2019-12-17 17:18:48 +0100

    add test for auth token relation

---
+++ tests/authToken.test.js
@@ -0,0 +1,31 @@
+const test = require('ava');
+const { close, init } = require('./index');
+
+test.before(async t => {
+    await init();
+    const { AuthToken, User } = require('../models');
+    const user = await User.findByPk(1);
+    t.context = { AuthToken, user };
+});
+
+test('log a new AuthToken', async t => {
+    const { AuthToken, user } = t.context;
+
+    const res = await AuthToken.newToken({
+        user_id: user.id,
+        comment: 'orm-test/run'
+    });
+
+    t.is(res.comment, 'orm-test/run');
+    t.is(typeof res.token, 'string');
+    t.is(res.token.length, 32);
+    t.is(res.user_id, user.id);
+
+    await AuthToken.destroy({
+        where: {
+            key: 'orm-test/run'
+        }
+    });
+});
+
+test.after(t => close);
