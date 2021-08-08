6be7f7aae456b1e862c37afb617b4ae2a642a066
Gregor Aisch <gka@users.noreply.github.com>
2021-08-08 09:43:35 +0200

    add themes.owner_id

--- models/assoc/user-theme.js
+++ models/assoc/user-theme.js
@@ -1,11 +1,16 @@
 const User = require('../User');
 const Theme = require('../Theme');
 
+// every theme must have an owner
+Theme.belongsTo(User, { foreignKey: 'owner_id' });
+
+// users can have (read) access to many themes
 User.belongsToMany(Theme, {
     through: 'user_theme',
     timestamps: false
 });
 
+// themes can be readable by many users
 Theme.belongsToMany(User, {
     through: 'user_theme',
     timestamps: false
